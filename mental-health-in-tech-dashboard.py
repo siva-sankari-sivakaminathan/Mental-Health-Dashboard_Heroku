e7c4afb2e9c639c9f7a25929574250862f19b89a
Siva Sankari Sivakaminathan <sankari.s2009@gmail.com>
2025-02-06 12:07:42 +0000

    Update mental-health-in-tech-dashboard.py

--- mental-health-in-tech-dashboard.py
+++ mental-health-in-tech-dashboard.py
@@ -1,30 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
-
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
-import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
-
-
-
-
 import pandas as pd
 import numpy as np
 import warnings
@@ -167,13 +140,16 @@ nulls_df[nulls_df['Nulls_Count'] > 0]
 # ------------------------------------------------------- CLEANING THE DATA -------------------------------------------------------
 # Column rename
 df = survey.copy()
+
 renamed_columns = ['tech_comp_flag', 'tech_role_flag', 'mh_employer_discussion_or_wellness_campaign', 'mh_resources_provided', 'mh_issue_request_medical_leave',
 'ph_employer_rate', 'mh_employer_rate', 'curr_have_mh_disorder', 'past_have_mh_disorder', 'fam_history', 
 'how often do you feel that it interferes with your work when being treated effectively?', 'smone_disscussion_reveal_ur_mh', 'admit_ph_issue_in_interview',
 'admit_mh_issue_in_interview', 'openly_id_as_person_wth_mh_issue', 'rate_tech_industry_support',
 'Briefly describe what you think the industry as a whole and/or employers could do to improve mental health support for employees.', 
 'age', 'gender', 'country_live_in', 'country_work_in', 'year']
+
 df.columns = renamed_columns
+
 # gender column needs to be recoded (number of unique values = 70)
 df['gender'].replace(to_replace = ['Male', 'male', 'Male ', 'M', 'm',
        'man', 'Cis male', 'Male.', 'male 9:1 female, roughly', 'Male (cis)', 'Man', 'Sex is male',
